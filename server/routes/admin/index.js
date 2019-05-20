9438e9e2ef470aeb7e59f3fa3a9d73cac721fd20
LuoXinghui <2440336395@qq.com>
2019-05-20 23:47:56 +0800

    修改为线上代码

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -42,7 +42,7 @@ module.exports = app => {
   })
   app.post('/admin/api/upload', upload.single('file'), async (req, res) => {
     const file = req.file
-    file.url = `http://localhost:3000/uploads/${file.filename}`
+    file.url = `http://39.108.13.204/uploads/${file.filename}`
     res.send(file)
   })
 
